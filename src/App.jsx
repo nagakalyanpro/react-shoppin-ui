10ba3f0ec6aa603c84a5839954c4498c07a03fb6
nagakalyanpro <77896080+nagakalyanpro@users.noreply.github.com>
2023-07-25 22:46:10 +0530

    shopping react ui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+import './App.css'
+import MainPage from './shoppingfolder/pages/MainPage'
+
+const App = () => {
+  return (
+    <div>
+    
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
